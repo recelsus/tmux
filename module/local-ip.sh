650f9c95bc1f49d4e790be9fc6d7c68756b19cc1
recelsus <rhinfunc+git@gmail.com>
2025-01-11 14:50:45 +0900

    Fix: Handle multiple network interfaces on macOS
    This commit modifies the local IP address retrieval on macOS to handle cases where multiple interfaces (e.g., en0, en1) might be active.
    
    - module/local-ip.sh
       Changed the IP address retrieval command to include both `en0` and `en1` interfaces.

--- module/local-ip.sh
+++ module/local-ip.sh
@@ -7,7 +7,7 @@ os=$(uname -s)
 
 if [ $os = 'Darwin' ]; then
   device='en0'
-  ip=$(ipconfig getifaddr en0)
+  ip=$(ipconfig getifaddr en0 | ipconfig getifaddr en1)
 elif [ $os = 'Linux' ]; then
   device=$(ip -4 -br a | grep UP | awk '{print $1}' | cut -f1 -d'/' | head -n 1)
   ip=$(ip -4 -br a | grep UP | awk '{print $3}' | cut -f1 -d'/' | head -n 1)
